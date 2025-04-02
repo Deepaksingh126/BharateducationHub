d2cd99330bdd00c7003229924c360d25f2944920
deepak singh <lalmohansingh83@gmail.com>
2025-04-02 20:04:18 +0530

    Fixed vite.config.js and other updates

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  base: "/",  // Ensure correct base path
+  server: {
+    historyApiFallback: true, // Fix for Vercel 404 error
+  }
 })
